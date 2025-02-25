b65b19f6ca2661086c8cc0add812dd58ef4144e5
Sir Cheesecake Johnson Turbo <cheesecakejohnson1@gmail.com>
2025-02-25 00:00:04 -0500

    This is the setup file in question
    It's a .bat so be sure that you review code and confirm that it matches your directory structure and that you have properly installed python 3.12.X in your user folder so VENV can sample from it.

---
+++ SetupVenvOnlyPYTHON312.bat
@@ -0,0 +1,30 @@
+@echo off
+SETLOCAL
+cd..
+cd comfyui
+    (
+    echo @echo off
+    echo set PYTHON=venv\Scripts\python.exe
+    echo set VENV_DIR=.\venv
+    echo %%PYTHON%% main.py --verbose %%*
+) > LaunchVENVed.bat
+::Make sure that you have installed python 3.12.X first
+"%USERPROFILE%\AppData\Local\Programs\Python\Python312\python.exe" -m venv venv
+call venv\scripts\activate
+:: Define the target file and shortcut location
+set targetFile=%~dp0ComfyUI\LaunchVENVed.bat
+set shortcutLocation=%~dp0LaunchVENVed.lnk
+:: Create a VBScript to generate the shortcut
+echo Set WshShell = WScript.CreateObject("WScript.Shell") > "%temp%\CreateShortcut.vbs"
+echo Set Shortcut = WshShell.CreateShortcut("%shortcutLocation%") >> "%temp%\CreateShortcut.vbs"
+echo Shortcut.TargetPath = "%targetFile%" >> "%temp%\CreateShortcut.vbs"
+echo Shortcut.WorkingDirectory = "%~dp0ComfyUI" >> "%temp%\CreateShortcut.vbs"
+echo Shortcut.WindowStyle = 1 >> "%temp%\CreateShortcut.vbs"
+echo Shortcut.Save >> "%temp%\CreateShortcut.vbs"
+:: Run the VBScript
+cscript //nologo "%temp%\CreateShortcut.vbs"
+:: Clean up the temporary VBScript
+del "%temp%\CreateShortcut.vbs"
+ENDLOCAL
+pause
+conda deactivate
